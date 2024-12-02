91484c896d72b29a02de356692d76cd48a357d48
Lalitha307 <21A91A05D2@aec.edu.in>
2024-12-02 20:15:29 +0530

    Add: Employee Attendance Feature

--- src/EmployeeWageComputation.java
+++ src/EmployeeWageComputation.java
@@ -1,5 +1,11 @@
 public class EmployeeWageComputation {
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Computation Program");
+        int IS_PRESENT = 1;
+        int attendance = (int) (Math.random() * 2);
+        if (attendance == IS_PRESENT) {
+            System.out.println("Employee is Present");
+        } else {
+            System.out.println("Employee is Absent");
+        }
     }
 }
