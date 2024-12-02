64139277c3e5d65e98dae410e45eb25739becf39
Lalitha307 <21A91A05D2@aec.edu.in>
2024-12-02 20:23:56 +0530

    Add: Switch Case Implementation for Wage Calculation

--- src/EmployeeWageComputation.java
+++ src/EmployeeWageComputation.java
@@ -1,8 +1,21 @@
 public class EmployeeWageComputation {
     public static void main(String[] args) {
-        int WAGE_PER_HOUR = 20;
-        int PART_TIME_HOUR = 4;
-        int partTimeWage = WAGE_PER_HOUR * PART_TIME_HOUR;
-        System.out.println("Part-Time Employee Wage: " + partTimeWage);
+        final int FULL_TIME = 1;
+        final int PART_TIME = 2;
+        final int WAGE_PER_HOUR = 20;
+        int workHours = 0;
+        int attendance = (int) (Math.random() * 3);
+        switch (attendance) {
+            case FULL_TIME:
+                workHours = 8;
+                break;
+            case PART_TIME:
+                workHours = 4;
+                break;
+            default:
+                workHours = 0;
+        }
+        int dailyWage = workHours * WAGE_PER_HOUR;
+        System.out.println("Employee Wage: " + dailyWage);
     }
 }
