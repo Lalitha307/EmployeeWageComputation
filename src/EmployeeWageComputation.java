0066c44c12d347529d8e62bd643aa1361e013607
Lalitha307 <21A91A05D2@aec.edu.in>
2024-12-02 20:31:18 +0530

    Add: Wages Till Condition Calculation

--- src/EmployeeWageComputation.java
+++ src/EmployeeWageComputation.java
@@ -1,10 +1,18 @@
 public class EmployeeWageComputation {
     public static void main(String[] args) {
-        final int WORKING_DAYS = 20;
         final int WAGE_PER_HOUR = 20;
+        final int MAX_HOURS = 100;
+        final int MAX_DAYS = 20;
         final int FULL_DAY_HOUR = 8;
-
-        int monthlyWage = WORKING_DAYS * FULL_DAY_HOUR * WAGE_PER_HOUR;
-        System.out.println("Monthly Wage: " + monthlyWage);
+        final int PART_TIME_HOUR = 4;
+        int totalHours = 0, totalDays = 0, totalWage = 0;
+        while (totalHours < MAX_HOURS && totalDays < MAX_DAYS) {
+            int attendance = (int) (Math.random() * 3);
+            int workHours = attendance == 1 ? FULL_DAY_HOUR : attendance == 2 ? PART_TIME_HOUR : 0;
+            totalHours += workHours;
+            totalDays++;
+            totalWage += workHours * WAGE_PER_HOUR;
+        }
+        System.out.println("Total Wage: " + totalWage);
     }
 }
