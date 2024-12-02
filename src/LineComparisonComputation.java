c3532da783abc2dcf8f58459eb4cf7875ec637bf
Lalitha307 <21A91A05D2@aec.edu.in>
2024-12-02 20:42:40 +0530

    Add: UC2 - Compare Two Lines

--- src/LineComparisonComputation.java
+++ src/LineComparisonComputation.java
@@ -1,9 +1,9 @@
 public class LineComparisonComputation {
     public static double calculateLength(int x1, int y1, int x2, int y2) {
         return Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
-    }
-    public static boolean areLinesEqual(double length1, double length2) {
-        return length1 == length2;
+    }// Method to compare lengths of two lines
+    public static int compareLines(double length1, double length2) {
+        return Double.compare(length1, length2);
     }
     public static void main(String[] args) {
         int x1 = 1, y1 = 1, x2 = 4, y2 = 5;
@@ -12,10 +12,13 @@ public class LineComparisonComputation {
         double length2 = calculateLength(x3, y3, x4, y4);
         System.out.println("Length of Line 1: " + length1);
         System.out.println("Length of Line 2: " + length2);
-        if (areLinesEqual(length1, length2)) {
+        int result = compareLines(length1, length2);
+        if (result == 0) {
             System.out.println("The two lines are equal.");
+        } else if (result > 0) {
+            System.out.println("Line 1 is greater than Line 2.");
         } else {
-            System.out.println("The two lines are not equal.");
+            System.out.println("Line 2 is greater than Line 1.");
         }
     }
 }
