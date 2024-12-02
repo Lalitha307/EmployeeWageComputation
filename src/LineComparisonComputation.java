e62d17ba2c2d0fa258ecdf218596260a9c71c91b
Lalitha307 <21A91A05D2@aec.edu.in>
2024-12-02 20:56:10 +0530

    Add: UC6 - Compare Multiple Lines and Calculate Longest, Shortest, and Average Lengths

--- src/LineComparisonComputation.java
+++ src/LineComparisonComputation.java
@@ -1,24 +1,60 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class LineComparisonComputation {
+    public static double calculateLength(int x1, int y1, int x2, int y2) {
+        return Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+    }
     public static int compareLines(double length1, double length2) {
         return Double.compare(length1, length2);  // Returns 0, 1, or -1
     }
-    public static double calculateWagesUntilCondition(double wagePerHour, int hoursPerDay, int maxHours, int maxDays) {
-        int totalHours = 0;
-        int totalDays = 0;
-        double totalWage = 0;
-        while (totalHours < maxHours && totalDays < maxDays) {
-            totalHours += hoursPerDay;
-            totalDays++;
-            totalWage += wagePerHour * hoursPerDay;
+    public static double findLongestLine(List<Double> lengths) {
+        double longest = lengths.get(0);
+        for (double length : lengths) {
+            if (length > longest) {
+                longest = length;
+            }
         }
-        return totalWage;
+        return longest;
+    }
+    public static double findShortestLine(List<Double> lengths) {
+        double shortest = lengths.get(0);
+        for (double length : lengths) {
+            if (length < shortest) {
+                shortest = length;
+            }
+        }
+        return shortest;
+    }
+    public static double calculateAverageLength(List<Double> lengths) {
+        double sum = 0;
+        for (double length : lengths) {
+            sum += length;
+        }
+        return sum / lengths.size();
     }
     public static void main(String[] args) {
-        double wagePerHour = 20;
-        int hoursPerDay = 8;
-        int maxHours = 100;
-        int maxDays = 20;
-        double totalWage = calculateWagesUntilCondition(wagePerHour, hoursPerDay, maxHours, maxDays);
-        System.out.println("Total Wage: " + totalWage);
+        // List to store the lengths of the lines
+        List<Double> lineLengths = new ArrayList<>();
+        int[][] coordinates = {
+                {1, 1, 4, 5},   // Line 1
+                {2, 2, 5, 6},   // Line 2
+                {3, 3, 6, 7},   // Line 3
+                {4, 4, 7, 8}    // Line 4
+        };
+        for (int[] coord : coordinates) {
+            double length = calculateLength(coord[0], coord[1], coord[2], coord[3]);
+            lineLengths.add(length);
+        }
+        System.out.println("Lengths of all lines:");
+        for (double length : lineLengths) {
+            System.out.println(length);
+        }
+        double longest = findLongestLine(lineLengths);
+        double shortest = findShortestLine(lineLengths);
+        double average = calculateAverageLength(lineLengths);
+        System.out.println("Longest Line Length: " + longest);
+        System.out.println("Shortest Line Length: " + shortest);
+        System.out.println("Average Line Length: " + average);
     }
 }
