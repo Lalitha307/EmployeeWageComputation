5d7b851b9e446c559214a2f880899f4744f05daa
Lalitha307 <21A91A05D2@aec.edu.in>
2024-12-02 20:21:47 +0530

    Add: Part-Time Wage Calculation Feature

--- src/EmployeeWageComputation.java
+++ src/EmployeeWageComputation.java
@@ -1,8 +1,8 @@
 public class EmployeeWageComputation {
     public static void main(String[] args) {
         int WAGE_PER_HOUR = 20;
-        int FULL_DAY_HOUR = 8;
-        int dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
-        System.out.println("Daily Employee Wage: " + dailyWage);
+        int PART_TIME_HOUR = 4;
+        int partTimeWage = WAGE_PER_HOUR * PART_TIME_HOUR;
+        System.out.println("Part-Time Employee Wage: " + partTimeWage);
     }
 }
