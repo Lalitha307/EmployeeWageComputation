df094b3585d1c36f3ce867322de6255f25ed0ba5
Lalitha307 <21A91A05D2@aec.edu.in>
2024-12-02 20:26:38 +0530

    Add: Monthly Wage Calculation

--- src/EmployeeWageComputation.java
+++ src/EmployeeWageComputation.java
@@ -1,21 +1,10 @@
 public class EmployeeWageComputation {
     public static void main(String[] args) {
-        final int FULL_TIME = 1;
-        final int PART_TIME = 2;
+        final int WORKING_DAYS = 20;
         final int WAGE_PER_HOUR = 20;
-        int workHours = 0;
-        int attendance = (int) (Math.random() * 3);
-        switch (attendance) {
-            case FULL_TIME:
-                workHours = 8;
-                break;
-            case PART_TIME:
-                workHours = 4;
-                break;
-            default:
-                workHours = 0;
-        }
-        int dailyWage = workHours * WAGE_PER_HOUR;
-        System.out.println("Employee Wage: " + dailyWage);
+        final int FULL_DAY_HOUR = 8;
+
+        int monthlyWage = WORKING_DAYS * FULL_DAY_HOUR * WAGE_PER_HOUR;
+        System.out.println("Monthly Wage: " + monthlyWage);
     }
 }
