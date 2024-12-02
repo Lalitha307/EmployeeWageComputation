ed2df27d3be396b2274b2c1e810e3b3b2362acdc
Lalitha307 <21A91A05D2@aec.edu.in>
2024-12-02 20:53:04 +0530

    Add: UC5 - Calculate Wages Until 100 Hours or 20 Days

--- src/LineComparisonComputation.java
+++ src/LineComparisonComputation.java
@@ -1,18 +1,24 @@
 public class LineComparisonComputation {
-    public static double calculateLength(int x1, int y1, int x2, int y2) {
-        return Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
-    }
     public static int compareLines(double length1, double length2) {
         return Double.compare(length1, length2);  // Returns 0, 1, or -1
     }
-    public static double calculateMonthlyWage(double wagePerHour, int hoursPerDay, int workingDays) {
-        return wagePerHour * hoursPerDay * workingDays;
+    public static double calculateWagesUntilCondition(double wagePerHour, int hoursPerDay, int maxHours, int maxDays) {
+        int totalHours = 0;
+        int totalDays = 0;
+        double totalWage = 0;
+        while (totalHours < maxHours && totalDays < maxDays) {
+            totalHours += hoursPerDay;
+            totalDays++;
+            totalWage += wagePerHour * hoursPerDay;
+        }
+        return totalWage;
     }
     public static void main(String[] args) {
         double wagePerHour = 20;
         int hoursPerDay = 8;
-        int workingDays = 20;
-        double monthlyWage = calculateMonthlyWage(wagePerHour, hoursPerDay, workingDays);
-        System.out.println("Total Monthly Wage: " + monthlyWage);
+        int maxHours = 100;
+        int maxDays = 20;
+        double totalWage = calculateWagesUntilCondition(wagePerHour, hoursPerDay, maxHours, maxDays);
+        System.out.println("Total Wage: " + totalWage);
     }
 }
